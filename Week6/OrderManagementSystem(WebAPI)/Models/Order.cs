68f557d700324af9e5eb339c97796400ffe223fc
xiaoyi long <1260269038@qq.com>
2020-10-13 12:52:58 +0800

    2020.10.12

---
+++ Week6/OrderManagementSystem(WebAPI)/Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Models {
+  public class Order {
+    private static int count;
+
+    [Required]
+    public string Customer { get; set; }
+    [Key, Column(Order = 1)]
+    public string Id { get; set; }
+    public List<OrderItem> Items { get; set; }
+
+    public static string genID() {
+      var a = new StringBuilder(12);
+      foreach (var b in BitConverter.GetBytes(DateTime.Now.ToFileTime())) a.AppendFormat("{0:x2}", b);
+      foreach (var b in BitConverter.GetBytes(count)) a.AppendFormat("{0:x2}", b);
+
+      count++;
+      return a.ToString();
+    }
+  }
+}
---
+++ Week6/OrderManagementSystem(WebAPI)/Models/OrderItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Models {
+  public class OrderItem {
+    private double _amount;
+    private double _price;
+
+    public string OrderId { get; set; }
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public double Price {
+      get => _price;
+      set {
+        if (value > 0) {
+          _price = value;
+        }
+        else {
+          throw new InvalidDataException("价格不能位负数");
+        }
+      }
+    }
+
+    [Required]
+    public double Amount {
+      get => _amount;
+      set {
+        if (value > 0) {
+          _amount = value;
+        }
+        else {
+          throw new InvalidDataException("数量不能为负数");
+        }
+      }
+    }
+  }
+}
